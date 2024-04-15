ea8e56283653f2b9f2bcef5b38ae707b579c22e4
Charles Merrick Moss <merrickm@live.com>
2024-04-14 21:00:37 -0400

    Update Twitch OAuth logging and token validation

--- src/functions/twitchOAuth.js
+++ src/functions/twitchOAuth.js
@@ -5,11 +5,11 @@ app.http('twitchOAuth', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}" to oAuth twitch account`);
 
         context.info("Requesting Authorization from user...");
         const data = axios.post(`https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${TWITCH_CLIENT_ID}&redirect_uri=${TWITCH_OAUTH_REDIRECT}&scope=clips%3edit+user%3write%3chat+channel%3manage%3schedule+user%3write%3chat+user%3bot+channel%3manage%3broadcast`, {})
-        context.log("Data: " + data);
+        context.log("Data from first oAuth: " + data);
 
         return { body: `Hello.  The function is done executing.` };
     }
--- src/functions/twitchOAuthRedirect.js
+++ src/functions/twitchOAuthRedirect.js
@@ -1,23 +1,31 @@
-const { app } = require('@azure/functions');
+const { app, output } = require('@azure/functions');
 const axios = require('axios');
+
+const cosmosOutput = output.cosmosDB({
+    databaseName: "playdatesBot",
+    collectionName: "twitchAuthorization",
+    createIfNotExists: true,
+    connectionStringsetting: process.env.CosmosDbConnectionSetting
+})
 
 app.http('twitchOAuthRedirect', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
+    return: cosmosOutput,
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}" to valiadte oAuth tokens...`);
 
         context.log("Checking for errors...")
         if (request.query.get('error') || request.query.get('error_description')) {
             context.error(`Error: ${request.query.get('error')}, ${request.query.get('error_description')}`);
-            return { body: "Authorization denied. :("}
+            //return { body: "Authorization denied. :("}
+            return;
         }
         
         const code = request.query.get('code');
-        const state = request.query.get('state');
         const scope = request.query.get('scope');
         
-        context.log("Requesting Access Token from Twitch...")
+        context.log("Requesting Access Token from Twitch using granted code...")
         const data = await axios.post(`https://id.twitch.tv/oauth2/token`, {
             client_id: process.env.TWITCH_CLIENT_ID,
             client_secret: process.env.TWITCH_CLIENT_SECRET,
@@ -29,9 +37,33 @@ app.http('twitchOAuthRedirect', {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
+        context.log("Data of tokens: " + JSON.stringify(data.data));
 
-        //SEND INFORMATION TO STORE IN COSMOSDB
+        context.log("Validating token...");
+        const tokenValidation = await axios.get(`https://id.twitch.tv/oauth2/validate`, {
+            headers: {
+                'Authorization': `OAuth ${data.data.access_token}`
+            }
+        });
+        context.log("Token Validation: " + JSON.stringify(tokenValidation.data));
+        
+        if (tokenValidation.status !== 200) {
+            context.error("Token validation failed.");
+            return;
+        }
 
-        return { body: `Hello, the script has finished executing!` };
+        context.log('Token is valid. Adding account data to CosmosDB...')
+        const accountData = JSON.parse({
+            twitchUserId: tokenValidation.data.user_id,
+            login: tokenValidation.data.login,
+            access_token: data.data.access_token,
+            refresh_token: data.data.refresh_token,
+            scope: scope, 
+            token_type: data.data.token_type
+        });
+        context.log("Account Data: " + JSON.stringify(accountData));
+
+        context.log("Adding account data to CosmosDB...");
+        return accountData;
     }
 });
