abc6a682c86f44ba3e40aff631f2d026b5b75ea1
Charles Merrick Moss <merrickm@live.com>
2024-04-15 22:00:49 -0400

    Update package-lock.json and twitchOAuth.js

--- src/functions/twitchOAuth.js
+++ src/functions/twitchOAuth.js
@@ -7,10 +7,6 @@ app.http('twitchOAuth', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        context.info("Requesting Authorization from user...");
-        const data = axios.post(`https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${TWITCH_CLIENT_ID}&redirect_uri=${TWITCH_OAUTH_REDIRECT}&scope=clips%3edit+user%3write%3chat+channel%3manage%3schedule+user%3write%3chat+user%3bot+channel%3manage%3broadcast`, {})
-        context.log("Data: " + data);
-
-        return { body: `Hello.  The function is done executing.` };
+        return { status: 302, headers: { location: `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${process.env.TWITCH_CLIENT_ID}&redirect_uri=${process.env.TWITCH_OAUTH_REDIRECT}&scope=clips%3Aedit+user%3Awrite%3Achat+channel%3Amanage%3Aschedule+user%3Awrite%3Achat+user%3Abot+channel%3Amanage%3Abroadcast` }};
     }
 });
