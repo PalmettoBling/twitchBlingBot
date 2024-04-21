c6630c077348bbb3567f7853a839c523e36bea5f
Charles Merrick Moss <merrickm@live.com>
2024-04-21 10:25:51 -0400

    Add appAccessToken function for Twitch OAuth authorization in appAccessToken.js

---
+++ src/functions/appAccessToken.js
@@ -0,0 +1,24 @@
+const { app } = require('@azure/functions');
+const axios = require('axios');
+
+app.http('appAccessToken', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const data = await axios.post('https://id.twitch.tv/oauth2/token', {
+            client_id: process.env.TWITCH_CLIENT_ID,
+            client_secret: process.env.TWITCH_CLIENT_SECRET,
+            grant_type: 'client_credentials'
+        }, {
+            headers: {
+                'Content-Type': 'application/x-www.form-urlencoded'
+            }
+        });
+        
+        // authorization is returned access token is reerenced: data.data.access_token
+
+        return {  };
+    }
+});
