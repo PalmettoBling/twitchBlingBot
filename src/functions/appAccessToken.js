8ef685d10b32a3152ec86abbd0a5317a63338509
Charles Merrick Moss <merrickm@live.com>
2024-04-21 10:26:41 -0400

    Refactor Twitch OAuth authorization process in appAccessToken.js

--- src/functions/appAccessToken.js
+++ src/functions/appAccessToken.js
@@ -7,7 +7,7 @@ app.http('appAccessToken', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const data = await axios.post('https://id.twitch.tv/oauth2/token', {
+        const appToken = await axios.post('https://id.twitch.tv/oauth2/token', {
             client_id: process.env.TWITCH_CLIENT_ID,
             client_secret: process.env.TWITCH_CLIENT_SECRET,
             grant_type: 'client_credentials'
