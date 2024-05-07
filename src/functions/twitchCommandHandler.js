725d6d146890f6ddfa87e9215ce6c20f7a2cbf20
Charles Merrick Moss <merrickm@live.com>
2024-05-07 18:38:56 -0400

    chore: Update package.json and package-lock.json with new dependencies

--- src/functions/twitchCommandHandler.js
+++ src/functions/twitchCommandHandler.js
@@ -1,4 +1,5 @@
 const { app } = require('@azure/functions');
+const { crypto } = require('crypto');
 
 app.http('twitchCommandHandler', {
     methods: ['GET', 'POST'],
@@ -6,8 +7,49 @@ app.http('twitchCommandHandler', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        // Getting Headers and body from request
+        // Body is also parsed into an object for reference
+        const signature = await request.headers.get('Twitch-Eventsub-Message-Signature').toLowerCase();
+        const timestamp = await request.headers.get('Twitch-Eventsub-Message-Timestamp').toLowerCase();
+        const messageId = await request.headers.get('Twitch-Eventsub-Message-Id').toLowerCase();
+        const messageType = await request.headers.get('Twitch-Eventsub-Message-Type').toLowerCase();
+        const body = await request.text();
+        const bodyObject = JSON.parse(body);
+        context.info("Request body: " + body);
 
-        return { body: `Hello, ${name}!` };
+        // Getting message and secret
+        const message = `${messageId}${timestamp}${body}`;
+        let hmac = `sha256=` + crypto.createHmac('sha256', process.env.TWITCH_EVENTSUB_SECRET).update(message).digest('hex');
+
+        // Verifying signature
+        const verify = crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(signature));
+
+        // If not verified, returning 4xx error
+        if (!verify) {
+            context.error("Signature verification failed.");
+            return { status: 403, body: "Signature verification failed." };
+        }
+
+        // Challenge and response (ping pong)
+        if (messageType === 'webhook_callback_verification') {
+            return { body: bodyObject.challenge };
+        } else if (messageType === 'revocation') {
+            context.log("Subscription revoked: " + JSON.stringify(bodyObject.subscription.status));
+            return { status: 204 };
+        }
+
+        context.info("Signature verified.");
+        context.info("Notification: " + JSON.stringify(bodyObject));
+
+        // Check if notificaiton, if not, raise WTF flag
+        if (messageType !== 'notification') {
+            context.error("Unknown message type: " + messageType);
+            return { status: 204, body: "Unknown message type." };
+        }
+
+        // Process notification, sending to other functions?
+        
+
+        return { body: `Function successfully ran` };
     }
 });
