78a4432cc58f219cf927e96598b7c7b2175f9b8e
Charles Merrick Moss <merrickm@live.com>
2024-05-27 20:29:27 -0400

    refactor: Improve signature verification in twitchCommandHandler.js

--- src/functions/twitchCommandHandler.js
+++ src/functions/twitchCommandHandler.js
@@ -9,7 +9,7 @@ app.http('twitchCommandHandler', {
 
         // Getting Headers and body from request
         // Body is also parsed into an object for reference
-        const signature = request.headers.get('Twitch-Eventsub-Message-Signature').toLowerCase();
+        const signature = request.headers.get('Twitch-Eventsub-Message-Signature');
         const timestamp = request.headers.get('Twitch-Eventsub-Message-Timestamp').toLowerCase();
         const messageId = request.headers.get('Twitch-Eventsub-Message-Id').toLowerCase();
         const messageType = request.headers.get('Twitch-Eventsub-Message-Type').toLowerCase();
@@ -22,14 +22,10 @@ app.http('twitchCommandHandler', {
         context.info("Request body: " + body);
         context.info("Message Type: " + messageType);
         
-        let secret = process.env.TWITCH_WEBHOOK_SECRET;
-        let message = messageId + timestamp + body;
-        let hmac = 'sha256=' + getHmac(process.env.TWITCH_WEBHOOK_SECRET, message);
+        const message = timestamp + messageId + body;
+        const hmac = 'sha256=' + crypto.createHmac('sha256', process.env.TWITCH_WEBHOOK_SECRET).update(Buffer.from(message)).digest('hex');
         
-        if (false === verifyMessage(hmac, signature)) {
-            context.error("Message could not be verified");
-            return { status: 403, body: `Message could not be verified` };
-        } else {
+        if (true === crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(signature))) {
             context.info("Message verified");
 
             if (messageType === 'webhook_callback_verification') {
@@ -47,16 +43,11 @@ app.http('twitchCommandHandler', {
                 context.info("Condition: " + JSON.stringify(bodyObject.subscription.condition));
                 // Handle subscription revocation
             }
+        } else {
+            context.error("Message could not be verified");
+            return { status: 403, body: `Message could not be verified` };
         }  
         
         return { body: `Function successfully ran` };
     }
 });
-
-function getHmac(secret, message) {
-    return crypto.createHmac('sha256', secret).update(message).digest('hex');
-}
-
-function verifyMessage(hmac, signature) {
-    return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(signature));
-}
